8e40636a284a3a3de405f5c3c12fc60d6df4ccfa
Leodanis Pozo Ramos <lpozor78@gmail.com>
2020-02-07 17:49:59 -0500

    Add tests for viscosity.py

--- tests/test_viscosity.py
+++ tests/test_viscosity.py
@@ -23,6 +23,9 @@
 """This module provides tests for lubricalc package."""
 
 from lubepy.viscosity import viscosity_at_40
+from lubepy.viscosity import viscosity_at_100
+from lubepy.viscosity import viscosity
+from lubepy.viscosity import viscosity_index
 
 
 class TestViscosity:
@@ -31,56 +34,11 @@ class TestViscosity:
     def test_viscosity_at_40(self):
         assert viscosity_at_40(15, index=130) == 119.55
 
-    # def test_viscosity_index_156(self):
-    #     assert Viscosity().viscosity_index(22.83, 5.05) == 156
+    def test_viscosity_at_100(self):
+        assert viscosity_at_100(112, index=140) == 15.11
 
-    # def test_viscosity_index_92(self):
-    #     assert Viscosity().viscosity_index(73.3, 8.86) == 92
+    def test_viscosity(self):
+        assert viscosity(4.6, 2, 20) == 6.89
 
-    # def test_viscosity_index_145(self):
-    #     assert Viscosity().viscosity_index(138.9, 18.1) == 145
-
-    # def test_viscosity_index_string_input(self):
-    #     assert Viscosity().viscosity_index('138.9', '18.1') == 145
-
-    # def test_viscosity_index_coma_input(self):
-    #     assert Viscosity().viscosity_index('138,9', '18,1') == 145
-
-    # @nose.tools.raises(ValueError)
-    # def test_viscosity_index_inf_input(self):
-    #     Viscosity().viscosity_index(float('inf'), '18,1')
-
-    # @nose.tools.raises(ConceptError)
-    # def test_viscosity_index_cero_kv40_input(self):
-    #     Viscosity().viscosity_index(0, '18,1')
-
-    # @nose.tools.raises(ConceptError)
-    # def test_viscosity_index_cero_kv100_input(self):
-    #     Viscosity().viscosity_index(150, 0)
-
-    # @nose.tools.raises(InvertedViscosityError)
-    # def test_viscosity_index_kv40_lt_kv100_input(self):
-    #     Viscosity().viscosity_index(15, 150)
-
-    # @nose.tools.raises(ConceptError)
-    # def test_viscosity_index_input_lt_2(self):
-    #     Viscosity().viscosity_index(1.5, 1)
-
-    # def test_viscosity_at_100(self):
-    #     assert Viscosity().viscosity_at_100(112, v_index=140) == 15.12
-
-    # def test_viscosity_at_20_iso5(self):
-    #     assert Viscosity().viscosity_at_any_temp(4.6, 2, 20) == 6.89
-
-    # def test_viscosity_at_20_iso46(self):
-    #     assert Viscosity().viscosity_at_any_temp(46, 7, 20) == 130.66
-
-    # def test_viscosity_at_35_iso46(self):
-    #     assert Viscosity().viscosity_at_any_temp(46, 7, 35.0) == 58.08
-
-    # def test_viscosity_at_35_iso46_string_input(self):
-    #     assert Viscosity().viscosity_at_any_temp('46', '7 ', '35.0') == 58.08
-
-    # @nose.tools.raises(ConceptError)
-    # def test_viscosity_lt_273_iso46(self):
-    #     Viscosity().viscosity_at_any_temp('46', '7 ', '-275')
+    def test_viscosity_index(self):
+        assert viscosity_index(22.83, 5.05) == 156
