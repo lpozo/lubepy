d465a058c19320c3cfbe6d24815473b6b5ccd223
Leodanis Pozo Ramos <lpozor78@gmail.com>
2020-02-08 16:14:25 -0500

    Improve annotations

--- lubepy/blend.py
+++ lubepy/blend.py
@@ -21,6 +21,8 @@
 
 """This module provides OilBlend Class."""
 
+from typing import Dict
+
 ASH_CONTRIB = {
     "zinc": 1.50,
     "barium": 1.70,
@@ -45,7 +47,9 @@ def additive_percent_mass(
     ).additive_percent_mass()
 
 
-def ash_per_metal(metal: str, metal_content: dict, additive_percent: float) -> float:
+def ash_per_metal(
+    metal: str, metal_content: Dict[str, float], additive_percent: float
+) -> float:
     """Calculate the % of Sulfated Ash (SA) of a motor oil."""
     return OilBlend(additive_percent, 0.0, 0.0, metal_content).ash_per_metal(metal)
 
@@ -57,6 +61,7 @@ def total_ash(metal_content: dict, additive_percent: float) -> float:
 
 class OilBlend:
     """Class to calculate some parameters of a motor oil blend."""
+
     ash_contrib: dict = ASH_CONTRIB
 
     def __init__(
@@ -64,8 +69,8 @@ class OilBlend:
         additive_percent: float,
         additive_density: float,
         oil_density: float,
-        metal_content: dict,
-    ):
+        metal_content: Dict[str, float],
+    ) -> None:
         """Class initializer.
 
         additive_percent: Total % of additive in the blend (% volume)
@@ -79,7 +84,7 @@ class OilBlend:
         self.oil_density = oil_density
         self.metal_content = metal_content
 
-    def additive_percent_mass(self):
+    def additive_percent_mass(self) -> float:
         """Calculate the % by mass of Additive in a motor oil.
 
                                 Additive Density (kg/L) * Additive (% volume)
@@ -109,6 +114,6 @@ class OilBlend:
             3,
         )
 
-    def total_ash(self):
+    def total_ash(self) -> float:
         """Calculate the total content of sulfated ash."""
         return round(sum(self.ash_per_metal(metal) for metal in self.metal_content), 2)
