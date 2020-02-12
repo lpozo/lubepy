759eccea15f75be16919b821859ccc58ba039f46
Leodanis Pozo Ramos <lpozor78@gmail.com>
2020-02-11 22:13:00 -0500

    Update tests

--- tests/test_reynols.py
+++ tests/test_reynols.py
@@ -22,26 +22,33 @@
 
 """This module provides tests for reynolds.py."""
 
-from lubepy.reynolds import Reynolds
-from lubepy.reynolds import reynolds_number
-from lubepy.reynolds import flow_type
-from lubepy.reynolds import FlowType
+import pytest
+from pytest import param
+
+from lubepy.reynolds import FlowType, Reynolds, flow_type, reynolds_number
 
 
 class TestReynolds:
     """Class to test Reynolds."""
 
-    def setup_method(self):
-        self.reynolds = Reynolds(velocity=15.0, length=0.10, viscosity=3.0)
-
     def test_reynolds_number(self):
-        assert self.reynolds.reynolds_number() == 0.5
+        reynolds = Reynolds(velocity=15.0, length=0.10, viscosity=3.0)
+        assert reynolds.reynolds_number() == 0.5
 
     def test_reynolds_number_func(self):
         assert reynolds_number(15.0, 0.10, 3) == 0.5
 
-    def test_reynolds_flow_type(self):
-        assert self.reynolds.flow_type() == FlowType.LAMINAR
+    @pytest.mark.parametrize(
+        "velocity, length, viscosity, expected",
+        [
+            param(15.0, 0.1, 3.0, FlowType.LAMINAR),
+            param(600.0, 20.0, 3.0, FlowType.TURBULENT),
+            param(600.0, 15.0, 3.0, FlowType.MIXED),
+        ],
+    )
+    def test_reynolds_flow_type(self, velocity, length, viscosity, expected):
+        reynolds = Reynolds(velocity, length, viscosity)
+        assert reynolds.flow_type() == expected
 
     def test_reynolds_flow_type_func(self):
         assert flow_type(15.0, 0.10, 3) == FlowType.LAMINAR
